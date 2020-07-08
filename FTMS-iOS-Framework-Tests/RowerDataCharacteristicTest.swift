ac6bf3f308ed46a5c2488102607de4c590916a7e
Niek Haarman <niek@305.nl>
2020-07-08 18:40:46 +0200

    Decode average stroke rate

--- FTMS-iOS-Framework-Tests/RowerDataCharacteristicTest.swift
+++ FTMS-iOS-Framework-Tests/RowerDataCharacteristicTest.swift
@@ -4,11 +4,29 @@ import XCTest
 
 class RowerDataCharacteristicTest: XCTestCase {
 
-    func test_decoding_empty_data_returns_nil() {
+    // MARK: Average Stroke Rate
+
+    func test_averageStrokeRate_not_present_results_in_nil_value() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent: false)
+        let data = CharacteristicData.create(flags: flags)
+
         /* When */
-        let result = RowerDataCharacteristic.decode(data: Data())
+        let result = RowerDataCharacteristic.decode(data: data)
 
         /* Then */
-        XCTAssertNil(result)
+        XCTAssertNil(result.averageStrokeRate)
+    }
+
+    func test_averageStrokeRate_present_results_in_uint8_value_with_binary_exponent_minusone() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent: true)
+        let data = CharacteristicData.create(flags: flags, values: 7)
+
+        /* When */
+        let result = RowerDataCharacteristic.decode(data: data)
+
+        /* Then */
+        XCTAssertEqual(result.averageStrokeRate, 3.5)
     }
 }
--- FTMS-iOS-Framework/RowerDataCharacteristic.swift
+++ FTMS-iOS-Framework/RowerDataCharacteristic.swift
@@ -1,8 +1,26 @@
 import Foundation
 
+struct RowerData {
+
+    let averageStrokeRate: Double?
+}
+
 class RowerDataCharacteristic {
 
-    static func decode(data: Data) -> Any? {
+    static func decode(data: Data) -> RowerData {
+        return RowerData(
+            averageStrokeRate: averageStrokeRate(from: data)
+        )
+    }
+
+    private static func averageStrokeRate(from data: Data) -> Double? {
+        let flagsValue = data[0]
+        let averageStrokeRatePresent = flagsValue & 0b10 != 0
+
+        if averageStrokeRatePresent {
+            return Double(data[2]) * (pow(2.0, -1.0))
+        }
+
         return nil
     }
 }
