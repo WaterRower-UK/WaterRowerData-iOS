652c1c13477a0811c1bce32a85e13a82890e16bb
Niek Haarman <niek@305.nl>
2020-07-08 17:04:28 +0200

    Remove xcodegen

--- FTMS-iOS-Framework-Tests/ExampleTest.swift
+++ FTMS-iOS-Framework-Tests/ExampleTest.swift
@@ -1,10 +1,11 @@
 import XCTest
 
-@testable import Pods_FTMS_iOS_Framework
+@testable import FTMS_iOS_Framework
 
 class ExampleTest: XCTestCase {
 
     func testExample() {
         XCTAssertEqual(2 + 3, 5)
+        XCTAssertEqual(Example().foo(), 3)
     }
 }
