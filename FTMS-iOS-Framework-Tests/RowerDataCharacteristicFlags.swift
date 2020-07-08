2c622f37eb2bf57e8179c9ddcb3641362e3493b6
Niek Haarman <niek@305.nl>
2020-07-08 18:18:27 +0200

    Set up RowerDataCharacteristicFlags with support for Average Stroke Rate

---
+++ FTMS-iOS-Framework-Tests/RowerDataCharacteristicFlags.swift
@@ -0,0 +1,30 @@
+import Foundation
+import XCTest
+
+class RowerDataCharacteristicFlags {
+
+    static func create(
+        averageStrokeRatePresent: Bool = false
+    ) -> Data {
+        var flags: [Int: Bool] = [:]
+
+        if averageStrokeRatePresent {
+            flags[1] = true
+        }
+
+        return CharacteristicFlags.createFlags(flags: flags)
+    }
+}
+
+class RowerDataCharacteristicFlagsTest: XCTestCase {
+
+    func test_averageStrokeRate_present() {
+        /* When */
+        let result = RowerDataCharacteristicFlags.create(
+            averageStrokeRatePresent: true
+        )
+
+        /* Then */
+        XCTAssertEqual(result[0], UInt8("00000010", radix: 2))
+    }
+}
