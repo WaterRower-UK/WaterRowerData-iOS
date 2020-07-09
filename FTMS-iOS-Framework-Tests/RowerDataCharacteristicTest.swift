37d00a59f0d520318f693b4cab6889f3c796ff65
Niek Haarman <niek@305.nl>
2020-07-09 10:46:32 +0200

    Decode total distance

--- FTMS-iOS-Framework-Tests/RowerDataCharacteristicTest.swift
+++ FTMS-iOS-Framework-Tests/RowerDataCharacteristicTest.swift
@@ -6,7 +6,7 @@ class RowerDataCharacteristicTest: XCTestCase {
 
     // MARK: Average Stroke Rate
 
-    func test_averageStrokeRate_not_present_results_in_nil_value() {
+    func test_averageStrokeRate_notPresent_resultsIn_nilValue() {
         /* Given */
         let flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent: false)
         let data = CharacteristicData.create(flags: flags)
@@ -18,7 +18,7 @@ class RowerDataCharacteristicTest: XCTestCase {
         XCTAssertNil(result.averageStrokeRate)
     }
 
-    func test_averageStrokeRate_present_results_in_uint8_value_with_binary_exponent_minusone() {
+    func test_averageStrokeRate_present_resultsIn_uint8Value_withBinaryExponentMinusOne() {
         /* Given */
         let flags = RowerDataCharacteristicFlags.create(averageStrokeRatePresent: true)
         let data = CharacteristicData.create(flags: flags, values: 7)
@@ -28,5 +28,55 @@ class RowerDataCharacteristicTest: XCTestCase {
 
         /* Then */
         XCTAssertEqual(result.averageStrokeRate, 3.5)
+    }
+
+    // MARK: Total Distance
+
+    func test_totalDistance_notPresent_resultsIn_nilValue() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(totalDistancePresent: false)
+        let data = CharacteristicData.create(flags: flags)
+
+        /* When */
+        let result = RowerDataCharacteristic.decode(data: data)
+
+        /* Then */
+        XCTAssertNil(result.totalDistanceMeters)
+    }
+
+    func test_totalDistance_present_resultsIn_uint24Value_forLowValue() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(totalDistancePresent: true)
+        let data = CharacteristicData.create(flags: flags, values: 1, 0, 0)
+
+        /* When */
+        let result = RowerDataCharacteristic.decode(data: data)
+
+        /* Then */
+        XCTAssertEqual(result.totalDistanceMeters, 1)
+    }
+
+    func test_totalDistance_present_resultsIn_uint24Value_forMediumValue() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(totalDistancePresent: true)
+        let data = CharacteristicData.create(flags: flags, values: 1, 2, 0) // 1 + 512
+
+        /* When */
+        let result = RowerDataCharacteristic.decode(data: data)
+
+        /* Then */
+        XCTAssertEqual(result.totalDistanceMeters, 513)
+    }
+
+    func test_totalDistance_present_resultsIn_uint24Value_forHighValue() {
+        /* Given */
+        let flags = RowerDataCharacteristicFlags.create(totalDistancePresent: true)
+        let data = CharacteristicData.create(flags: flags, values: 1, 2, 4) // 1 + 512 + 262144
+
+        /* When */
+        let result = RowerDataCharacteristic.decode(data: data)
+
+        /* Then */
+        XCTAssertEqual(result.totalDistanceMeters, 262657)
     }
 }
--- FTMS-iOS-Framework/RowerDataCharacteristic.swift
+++ FTMS-iOS-Framework/RowerDataCharacteristic.swift
@@ -3,13 +3,15 @@ import Foundation
 struct RowerData {
 
     let averageStrokeRate: Double?
+    let totalDistanceMeters: UInt32?
 }
 
 class RowerDataCharacteristic {
 
     static func decode(data: Data) -> RowerData {
         return RowerData(
-            averageStrokeRate: averageStrokeRate(from: data)
+            averageStrokeRate: averageStrokeRate(from: data),
+            totalDistanceMeters: totalDistanceMeters(from: data)
         )
     }
 
@@ -19,6 +21,17 @@ class RowerDataCharacteristic {
 
         if averageStrokeRatePresent {
             return Double(data[2]) * (pow(2.0, -1.0))
+        }
+
+        return nil
+    }
+
+    private static func totalDistanceMeters(from data: Data) -> UInt32? {
+        let flagsValue = data[0]
+        let totalDistancePresent = flagsValue & 0b100 != 0
+
+        if totalDistancePresent {
+            return UInt32(data[2]) + (UInt32(data[3]) << 8) + (UInt32(data[4]) << 16)
         }
 
         return nil
